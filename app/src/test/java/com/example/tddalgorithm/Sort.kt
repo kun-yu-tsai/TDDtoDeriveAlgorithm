a9fe974cf41674f67bd04ba71b616d9d154e4892
Kun <kun.register@gmail.com>
2019-05-27 08:37:28 +0100

    Implement another sort

--- app/src/test/java/com/example/tddalgorithm/Sort.kt
+++ app/src/test/java/com/example/tddalgorithm/Sort.kt
@@ -1,66 +1,72 @@
 package com.example.tddalgorithm
 
-import org.hamcrest.CoreMatchers
+import org.hamcrest.CoreMatchers.`is`
 import org.junit.Assert.assertThat
 import org.junit.Test
 
 class Sort {
     @Test
     fun test() {
-        assertThat(input(arrayOf(1)), CoreMatchers.`is`(arrayOf(1)))
+        assertThat(input(arrayOf(1)), `is`(arrayOf(1)))
 
-        assertThat(input(arrayOf(1, 2)), CoreMatchers.`is`(arrayOf(1, 2)))
-        assertThat(input(arrayOf(2, 1)), CoreMatchers.`is`(arrayOf(1, 2)))
+        assertThat(input(arrayOf(1, 2)), `is`(arrayOf(1, 2)))
+        assertThat(input(arrayOf(2, 1)), `is`(arrayOf(1, 2)))
 
-        assertThat("1", input(arrayOf(1, 2, 3)), CoreMatchers.`is`(arrayOf(1, 2, 3)))
-        assertThat(input(arrayOf(1, 3, 2)), CoreMatchers.`is`(arrayOf(1, 2, 3)))
-        assertThat(input(arrayOf(2, 1, 3)), CoreMatchers.`is`(arrayOf(1, 2, 3)))
-        assertThat("3", input(arrayOf(3, 1, 2)), CoreMatchers.`is`(arrayOf(1, 2, 3)))
-        assertThat("2", input(arrayOf(3, 2, 1)), CoreMatchers.`is`(arrayOf(1, 2, 3)))
+        assertThat(input(arrayOf(1, 2, 3)), `is`(arrayOf(1, 2, 3)))
+        assertThat(input(arrayOf(3, 2, 1)), `is`(arrayOf(1, 2, 3)))
+        assertThat(input(arrayOf(2, 1, 3)), `is`(arrayOf(1, 2, 3)))
+        assertThat(input(arrayOf(3, 1, 2)), `is`(arrayOf(1, 2, 3)))
+        assertThat(input(arrayOf(1, 3, 2)), `is`(arrayOf(1, 2, 3)))
+        assertThat(input(arrayOf(3, 2, 1)), `is`(arrayOf(1, 2, 3)))
 
-        assertThat(input(arrayOf(1, 2, 3, 4)), CoreMatchers.`is`(arrayOf(1, 2, 3, 4)))
-        assertThat(input(arrayOf(2, 1, 3, 4)), CoreMatchers.`is`(arrayOf(1, 2, 3, 4)))
-        assertThat(input(arrayOf(1, 3, 2, 4)), CoreMatchers.`is`(arrayOf(1, 2, 3, 4)))
-        assertThat(input(arrayOf(3, 1, 2, 4)), CoreMatchers.`is`(arrayOf(1, 2, 3, 4)))
-        assertThat(input(arrayOf(3, 2, 1, 4)), CoreMatchers.`is`(arrayOf(1, 2, 3, 4)))
+        assertThat(input(arrayOf(1, 2, 3, 4)), `is`(arrayOf(1, 2, 3, 4)))
+        assertThat(input(arrayOf(2, 1, 3, 4)), `is`(arrayOf(1, 2, 3, 4)))
+        assertThat(input(arrayOf(1, 3, 2, 4)), `is`(arrayOf(1, 2, 3, 4)))
+        assertThat(input(arrayOf(3, 1, 2, 4)), `is`(arrayOf(1, 2, 3, 4)))
+        assertThat(input(arrayOf(3, 2, 1, 4)), `is`(arrayOf(1, 2, 3, 4)))
 
-        assertThat(input(arrayOf(1, 2, 4, 3)), CoreMatchers.`is`(arrayOf(1, 2, 3, 4)))
-        assertThat(input(arrayOf(1, 3, 4, 2)), CoreMatchers.`is`(arrayOf(1, 2, 3, 4)))
+        assertThat(input(arrayOf(1, 2, 4, 3)), `is`(arrayOf(1, 2, 3, 4)))
+        assertThat(input(arrayOf(1, 3, 4, 2)), `is`(arrayOf(1, 2, 3, 4)))
 
-        assertThat(input(arrayOf(2, 1, 4, 3)), CoreMatchers.`is`(arrayOf(1, 2, 3, 4)))
-        assertThat(input(arrayOf(2, 3, 4, 1)), CoreMatchers.`is`(arrayOf(1, 2, 3, 4)))
+        assertThat(input(arrayOf(2, 1, 4, 3)), `is`(arrayOf(1, 2, 3, 4)))
+        assertThat(input(arrayOf(2, 3, 4, 1)), `is`(arrayOf(1, 2, 3, 4)))
 
-        assertThat(input(arrayOf(3, 1, 4, 2)), CoreMatchers.`is`(arrayOf(1, 2, 3, 4)))
-        assertThat(input(arrayOf(3, 2, 4, 1)), CoreMatchers.`is`(arrayOf(1, 2, 3, 4)))
+        assertThat(input(arrayOf(3, 1, 4, 2)), `is`(arrayOf(1, 2, 3, 4)))
+        assertThat(input(arrayOf(3, 2, 4, 1)), `is`(arrayOf(1, 2, 3, 4)))
 
 
-        assertThat(input(arrayOf(1, 4, 2, 3)), CoreMatchers.`is`(arrayOf(1, 2, 3, 4)))
-        assertThat(input(arrayOf(1, 4, 3, 2)), CoreMatchers.`is`(arrayOf(1, 2, 3, 4)))
+        assertThat(input(arrayOf(1, 4, 2, 3)), `is`(arrayOf(1, 2, 3, 4)))
+        assertThat(input(arrayOf(1, 4, 3, 2)), `is`(arrayOf(1, 2, 3, 4)))
 
-        assertThat(input(arrayOf(2, 4, 1, 3)), CoreMatchers.`is`(arrayOf(1, 2, 3, 4)))
-        assertThat(input(arrayOf(2, 4, 3, 1)), CoreMatchers.`is`(arrayOf(1, 2, 3, 4)))
+        assertThat(input(arrayOf(2, 4, 1, 3)), `is`(arrayOf(1, 2, 3, 4)))
+        assertThat(input(arrayOf(2, 4, 3, 1)), `is`(arrayOf(1, 2, 3, 4)))
 
-        assertThat(input(arrayOf(3, 4, 1, 2)), CoreMatchers.`is`(arrayOf(1, 2, 3, 4)))
-        assertThat(input(arrayOf(3, 4, 2, 1)), CoreMatchers.`is`(arrayOf(1, 2, 3, 4)))
+        assertThat(input(arrayOf(3, 4, 1, 2)), `is`(arrayOf(1, 2, 3, 4)))
+        assertThat(input(arrayOf(3, 4, 2, 1)), `is`(arrayOf(1, 2, 3, 4)))
 
-        assertThat(input(arrayOf(4, 1, 2, 3)), CoreMatchers.`is`(arrayOf(1, 2, 3, 4)))
-        assertThat(input(arrayOf(4, 1, 3, 2)), CoreMatchers.`is`(arrayOf(1, 2, 3, 4)))
+        assertThat(input(arrayOf(4, 1, 2, 3)), `is`(arrayOf(1, 2, 3, 4)))
+        assertThat(input(arrayOf(4, 1, 3, 2)), `is`(arrayOf(1, 2, 3, 4)))
 
-        assertThat(input(arrayOf(4, 2, 1, 3)), CoreMatchers.`is`(arrayOf(1, 2, 3, 4)))
-        assertThat(input(arrayOf(4, 2, 3, 1)), CoreMatchers.`is`(arrayOf(1, 2, 3, 4)))
+        assertThat(input(arrayOf(4, 2, 1, 3)), `is`(arrayOf(1, 2, 3, 4)))
+        assertThat(input(arrayOf(4, 2, 3, 1)), `is`(arrayOf(1, 2, 3, 4)))
 
-        assertThat(input(arrayOf(4, 3, 1, 2)), CoreMatchers.`is`(arrayOf(1, 2, 3, 4)))
-        assertThat(input(arrayOf(4, 3, 2, 1)), CoreMatchers.`is`(arrayOf(1, 2, 3, 4)))
+        assertThat(input(arrayOf(4, 3, 1, 2)), `is`(arrayOf(1, 2, 3, 4)))
+        assertThat(input(arrayOf(4, 3, 2, 1)), `is`(arrayOf(1, 2, 3, 4)))
 
-        assertThat(input(arrayOf(1, 2, 3, 4, 5)), CoreMatchers.`is`(arrayOf(1, 2, 3, 4, 5)))
-        assertThat(input(arrayOf(2, 1, 3, 4, 5)), CoreMatchers.`is`(arrayOf(1, 2, 3, 4, 5)))
-        assertThat(input(arrayOf(1, 3, 2, 4, 5)), CoreMatchers.`is`(arrayOf(1, 2, 3, 4, 5)))
-        assertThat(input(arrayOf(1, 2, 4, 3, 5)), CoreMatchers.`is`(arrayOf(1, 2, 3, 4, 5)))
-        assertThat(input(arrayOf(1, 4, 3, 2, 5)), CoreMatchers.`is`(arrayOf(1, 2, 3, 4, 5)))
+        assertThat(input(arrayOf(1, 2, 3, 4, 5)), `is`(arrayOf(1, 2, 3, 4, 5)))
+        assertThat(input(arrayOf(2, 1, 3, 4, 5)), `is`(arrayOf(1, 2, 3, 4, 5)))
+        assertThat(input(arrayOf(1, 3, 2, 4, 5)), `is`(arrayOf(1, 2, 3, 4, 5)))
+        assertThat(input(arrayOf(1, 2, 4, 3, 5)), `is`(arrayOf(1, 2, 3, 4, 5)))
+        assertThat(input(arrayOf(1, 4, 3, 2, 5)), `is`(arrayOf(1, 2, 3, 4, 5)))
 
-        assertThat(input(arrayOf(1, 2, 3, 5, 4)), CoreMatchers.`is`(arrayOf(1, 2, 3, 4, 5)))
-        assertThat(input(arrayOf(5, 2, 1, 4, 3)), CoreMatchers.`is`(arrayOf(1, 2, 3, 4, 5)))
-        assertThat(input(arrayOf(2, 5, 3, 1, 4)), CoreMatchers.`is`(arrayOf(1, 2, 3, 4, 5)))
+        assertThat(input(arrayOf(1, 2, 3, 5, 4)), `is`(arrayOf(1, 2, 3, 4, 5)))
+        assertThat(input(arrayOf(5, 2, 1, 4, 3)), `is`(arrayOf(1, 2, 3, 4, 5)))
+        assertThat(input(arrayOf(2, 5, 3, 1, 4)), `is`(arrayOf(1, 2, 3, 4, 5)))
+
+        assertThat(
+            input(arrayOf(45, 12, 85, 32, 89, 39, 69, 44, 42, 1, 6, 8)),
+            `is`(arrayOf(1, 6, 8, 12, 32, 39, 42, 44, 45, 69, 85, 89))
+        )
     }
 
     private fun input(array: Array<Int>): Array<Int>? {
@@ -68,6 +74,28 @@ class Sort {
 //        if (array[0] > array[array.lastIndex])
 //            swap(array, 0, array.lastIndex)
 
+        anotherSort(array)
+
+//        bubbleSort(array)
+
+        return array
+    }
+
+    private fun anotherSort(array: Array<Int>) {
+        var movingForwardCursor = 0
+
+        while (movingForwardCursor < array.lastIndex) {
+            var movingBackWardCursor = array.lastIndex
+            while (movingBackWardCursor != movingForwardCursor) {
+                if (array[movingForwardCursor] > array[movingBackWardCursor])
+                    swap(array, movingForwardCursor, movingBackWardCursor)
+                movingBackWardCursor--
+            }
+            movingForwardCursor++
+        }
+    }
+
+    private fun bubbleSort(array: Array<Int>) {
         var anchorKeepMovingToFinalIndex = 1
         while (anchorKeepMovingToFinalIndex <= array.lastIndex) {
             var anchorSwapWithPreviousPosition = anchorKeepMovingToFinalIndex
@@ -77,8 +105,6 @@ class Sort {
             }
             anchorKeepMovingToFinalIndex++
         }
-
-        return array
     }
 
     private fun swap(array: Array<Int>, a: Int, b: Int): Array<Int> {
@@ -90,7 +116,7 @@ class Sort {
 
     @Test
     fun testSwap() {
-        assertThat(swap(arrayOf(1, 2), 0, 1), CoreMatchers.`is`(arrayOf(2, 1)))
-        assertThat(swap(arrayOf(1, 2, 3), 1, 2), CoreMatchers.`is`(arrayOf(1, 3, 2)))
+        assertThat(swap(arrayOf(1, 2), 0, 1), `is`(arrayOf(2, 1)))
+        assertThat(swap(arrayOf(1, 2, 3), 1, 2), `is`(arrayOf(1, 3, 2)))
     }
 }
